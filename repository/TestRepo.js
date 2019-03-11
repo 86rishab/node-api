9959826f2ab7c6a4689fc0f33e84f911511f1065
Rishab <rishab.aggarwal@accenture.com>
2019-03-11 12:53:47 +0530

    creating repo js and calling it.

--- repository/TestRepo.js
+++ repository/TestRepo.js
@@ -1,5 +1,24 @@
-var repoTest = function(){
-return 'repo return';
+// EmpDetails models lives here
+var EmpDetails  = require('../db/models/empdetails');
+
+module.exports = {
+    saveEmpDetails: function(empdetails,callback) {
+       
+        console.log(" inside saveEmpDetails>>> ");
+        
+          empdetails.save(function(err) {
+            callback(err);
+        });         
+       
+    },
+    // continue adding more functions
+    otherFunction: function() {
+
+    }
 }
 
-module.exports = router;
+
+//var myFunc1 = function() { ... };
+//var myFunc2 = function() { ... };
+//exports.myFunc1 = myFunc1;
+//exports.myFunc2 = myFunc2;
--- routes/TestRoute.js
+++ routes/TestRoute.js
@@ -5,6 +5,8 @@ var multer  = require('multer');
 // create our router
 var router = express.Router();
 
+var empRepo = require('../repository/TestRepo.js');
+
 // create application/json parser
 var jsonParser = bodyParser.json();
 // create application/x-www-form-urlencoded parser
@@ -17,12 +19,21 @@ var EmpDetails  = require('../db/models/empdetails');
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
+	
 	res.json({ message: 'hooray! welcome to our api!' });	
 });
 
 
 router.post('/addEmpDetails', jsonParser, function(req, res) {
 	console.log("name>>> "+ req.body.name);
+	var errHandling = function (err){
+		if(err!==null && err.errors){
+		res.send(err);
+		}else{
+			res.json("user created!");
+		}
+	}
+	//creating empdetails object
 	var empdetails = new EmpDetails();
 	empdetails.name = req.body.name;
 	empdetails.age= req.body.age;
@@ -31,13 +42,8 @@ router.post('/addEmpDetails', jsonParser, function(req, res) {
 	empdetails.address.state = req.body.address.state;
 	empdetails.address.country = req.body.address.country;
 	console.log("name>>> "+ req.body.address.addressLine1);
-	empdetails.save(function(err) {
-		if (err)
-			res.send(err);
-
-		res.json({ message: 'Employee Created !!' });
-	});
-	//res.json(empdetails);	
+	empRepo.saveEmpDetails(empdetails,errHandling);
+		
 });
 // FILE UPLOAD
 router.post('/uploadFile', upload.single('test') ,function(req, res) {
